14b55a8421cb49b5d1d4d765422b230804e41bb7
ahdbenali <ahdbenali@gmail.com>
2025-09-07 00:18:43 +0200

    Update client file

--- db/client.ts
+++ db/client.ts
@@ -3,6 +3,7 @@ import {PrismaNeon} from '@prisma/adapter-neon'
 import {PrismaClient} from '~/lib/generated/prisma'
 // import {PrismaClient} from '@prisma/client'
 import ws from 'ws'
+import {de} from 'zod/v4/locales'
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 
@@ -15,7 +16,7 @@ if (!connectionString) {
 
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({connectionString})
+// const pool = new Pool({connectionString})
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 // const adapter = new PrismaNeon({connectionString})
@@ -23,7 +24,7 @@ const pool = new Pool({connectionString})
 
 // Use the pool's connection method
 // const adapter = new PrismaNeon({pool} )
-const adapter = new PrismaNeon({ connectionString })
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 // const prisma = new PrismaClient({adapter}).$extends({
@@ -43,27 +44,45 @@ const adapter = new PrismaNeon({ connectionString })
 //   },
 // })
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: connectionString,
-    },
-  },
-}).$extends({
+const prisma = new PrismaClient({adapter}).$extends({
   result: {
     product: {
       price: {
         compute(product) {
-          return product.price.toString();
+          return product.price.toString()
         },
       },
       rating: {
         compute(product) {
-          return product.rating.toString();
+          return product.rating.toString()
         },
       },
     },
   },
-});
+})
+
+
+// export const prisma = new PrismaClient({
+//   datasources: {
+//     db: {
+//       url: connectionString,
+//     },
+//   },
+// }).$extends({
+//   result: {
+//     product: {
+//       price: {
+//         compute(product) {
+//           return product.price.toString();
+//         },
+//       },
+//       rating: {
+//         compute(product) {
+//           return product.rating.toString();
+//         },
+//       },
+//     },
+//   },
+// });
 
 export {prisma as db}
